25a979699f870a5702d8d557f9a07e61ee4a8390
Gabriel Gutierri <98917672+GabrielGutierri@users.noreply.github.com>
2023-12-09 14:19:55 -0300

    feat: Refactoring frmCalculusInvaders
    Add some explicity to variables
    Created constants to facilitate the texts messages and, if there is some change, it will only happen in one place
    Created methods with a single responsability

---
+++ jogoN2v2.0/Constants/ConfigurationConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jogoN2v2._0.Constants
+{
+    public static class ConfigurationConstants
+    {
+        public const string MUSIC_ON = "on";
+        public const string MUSIC_OFF = "off";
+
+        public const string EASY = "Easy";
+        public const string NORMAL = "Normal";
+        public const string HARD = "Hard";
+
+    }
+}
