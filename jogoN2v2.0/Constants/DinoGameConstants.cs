45655c3a21ce3337667aada525f1664511cda927
Gabriel Gutierri <98917672+GabrielGutierri@users.noreply.github.com>
2023-12-10 12:26:04 -0300

    feat: Refactoring frmDinoGame
    Added some explicity to variables
    Created constants to facilitate the texts messages and, if there is some change, it will only happen in one place
    Created methods with a single responsability

---
+++ jogoN2v2.0/Constants/DinoGameConstants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jogoN2v2._0.Constants
+{
+    public static class DinoGameConstants
+    {
+        //messages
+        public const string INTRO_MESSAGE = "Press the spacebar to jump the integrals! Press R to restart";
+        public const string RESTART_MESSAGE = "Press R to restart";
+        public const string WIN_MESSAGE = "Congrats! You've won!";
+        public const string NO_LIFES_MESSAGE = "No more lifes! Your best round";
+        //tags
+        public const string OBSTACLE_TAG = "obstacle";
+
+        //tracks
+        public const string BACKGROUND_SOUND_TRACK = "FallGuys.mp3";
+        public const string HIT_SOUND_TRACK = "MarioHit.mp3";
+        public const string WIN_SOUND_TRACK = "winSound.mp3";
+        public const string GAMEOVER_SOUND_TRACK = "gameOver.mp3";
+        public const string JUMP_SOUND_TRACK = "jump.mp3";
+    }
+}
